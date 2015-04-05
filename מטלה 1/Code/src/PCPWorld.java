6b0ed4d230fd1c47785d95bfd4731d0077226d7c
noalevit18 <noalevit18@gmail.com>
2015-04-05 19:06:26 +0300

    בניתי קובץ טקסט מסודר המכיל את כל המצבים האפשריים, ממנו התוכנית קוראת ויוצרת את הסט של המצבים

--- מטלה 1/Code/src/PCPWorld.java
+++ מטלה 1/Code/src/PCPWorld.java
@@ -1,4 +1,9 @@
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 
@@ -7,16 +12,49 @@ public class PCPWorld {
 	private static final State InitialState = new State(false, 0, Disease.Unknown, 9);
 	private static Set<State>  states = new HashSet<State>();
 	
-	public PCPWorld()
+	public PCPWorld() throws IOException
 	{
-		states.add(InitialState);
+		List<String> lines=Files.readAllLines(Paths.get("states.txt"), Charset.forName("UTF-8"));
+		String[] data;
+		boolean didSurvive;
+		int timeInHospital, hour;
+		Disease disease;
+		for(String line:lines){
+			data = line.split(",");
+			if (data[0]=="false")
+				didSurvive= false;
+			else
+				didSurvive= true;
+			timeInHospital = Integer.parseInt(data[1]);
+			disease = getDisease(data[2]);
+			hour = Integer.parseInt(data[3]);
+			State newState = new State (didSurvive, timeInHospital, disease, hour);
+			states.add(newState);
+		}
 	}
 	
-	public State getInitialState() {
+	private Disease getDisease (String disease)
+	{
+		switch(disease)
+		{
+		case "flu":
+			return Disease.Flu;
+		case "cough":
+			return Disease.Cough;
+		case "ebole":
+			return Disease.Ebola;
+		default:
+			return Disease.Flu;
+		}
+	}
+	
+	public State getInitialState() 
+	{
 		return InitialState;
 	}
 
-	public Set<State> getStates() {	
+	public Set<State> getStates() 
+	{	
 		return states;				
 	}
 
