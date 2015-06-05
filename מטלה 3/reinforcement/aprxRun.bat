99c25a7cd75b492db0e1ba770dc3bb5935deac4d
eran yogev <yogeveran@gmail.com>
2015-06-06 00:20:33 +0300

    Updated results

--- מטלה 3/reinforcement/aprxRun.bat
+++ מטלה 3/reinforcement/aprxRun.bat
@@ -1,37 +1,65 @@
 @ECHO OFF
 setlocal EnableDelayedExpansion
-
+::========================================= epsilon 0.2
 ECHO epsilon=0.2,alpha=0.2 >> AllAproxResults.txt
 FOR /L %%N IN (1,2,10) DO (
 	set /a M = %%N + 3
 	ECHO Training %%N  >> AllAproxResults.txt
-	python pacman.py -p ApproximateQAgent -a extractor=SimpleExtractor,epsilon=0.2,alpha=0.2 -x %%N -n !M! -l mediumGrid |findstr "Win Rate" >> AllAproxResults.txt
+	python pacman.py -p ApproximateQAgent -a extractor=SimpleExtractor,epsilon=0.2,alpha=0.2 -x %%N -n !M! -l mediumClassic |findstr "Win Rate" >> AllAproxResults.txt
+)
+
+ECHO epsilon=0.2,alpha=0.5 >> AllAproxResults.txt
+FOR /L %%N IN (1,2,10) DO (
+	set /a M = %%N + 3
+	ECHO Training %%N  >> AllAproxResults.txt
+	python pacman.py -p ApproximateQAgent -a extractor=SimpleExtractor,epsilon=0.2,alpha=0.5 -x %%N -n !M! -l mediumClassic |findstr "Win Rate" >> AllAproxResults.txt
 )
 
 ECHO epsilon=0.2,alpha=0.8 >> AllAproxResults.txt
 FOR /L %%N IN (1,2,10) DO (
 	set /a M = %%N + 3
 	ECHO Training %%N  >> AllAproxResults.txt
-	python pacman.py -p ApproximateQAgent -a extractor=SimpleExtractor,epsilon=0.2,alpha=0.8 -x %%N -n !M! -l mediumGrid |findstr "Win Rate" >> AllAproxResults.txt
+	python pacman.py -p ApproximateQAgent -a extractor=SimpleExtractor,epsilon=0.2,alpha=0.8 -x %%N -n !M! -l mediumClassic |findstr "Win Rate" >> AllAproxResults.txt
 )
-
-ECHO epsilon=0.8,alpha=0.2 >> AllAproxResults.txt
+::========================================= epsilon 0.5
+ECHO epsilon=0.5,alpha=0.2 >> AllAproxResults.txt
 FOR /L %%N IN (1,2,10) DO (
 	set /a M = %%N + 3
 	ECHO Training %%N  >> AllAproxResults.txt
-	python pacman.py -p ApproximateQAgent -a extractor=SimpleExtractor,epsilon=0.8,alpha=0.2 -x %%N -n !M! -l mediumGrid |findstr "Win Rate" >> AllAproxResults.txt
+	python pacman.py -p ApproximateQAgent -a extractor=SimpleExtractor,epsilon=0.5,alpha=0.2 -x %%N -n !M! -l mediumClassic |findstr "Win Rate" >> AllAproxResults.txt
 )
 
 ECHO epsilon=0.5,alpha=0.5 >> AllAproxResults.txt
 FOR /L %%N IN (1,2,10) DO (
 	set /a M = %%N + 3
 	ECHO Training %%N  >> AllAproxResults.txt
-	python pacman.py -p ApproximateQAgent -a extractor=SimpleExtractor,epsilon=0.5,alpha=0.5 -x %%N -n !M! -l mediumGrid |findstr "Win Rate" >> AllAproxResults.txt
+	python pacman.py -p ApproximateQAgent -a extractor=SimpleExtractor,epsilon=0.5,alpha=0.5 -x %%N -n !M! -l mediumClassic |findstr "Win Rate" >> AllAproxResults.txt
+)
+
+ECHO epsilon=0.5,alpha=0.8 >> AllAproxResults.txt
+FOR /L %%N IN (1,2,10) DO (
+	set /a M = %%N + 3
+	ECHO Training %%N  >> AllAproxResults.txt
+	python pacman.py -p ApproximateQAgent -a extractor=SimpleExtractor,epsilon=0.5,alpha=0.8 -x %%N -n !M! -l mediumClassic |findstr "Win Rate" >> AllAproxResults.txt
+)
+::========================================= epsilon 0.8
+ECHO epsilon=0.8,alpha=0.2 >> AllAproxResults.txt
+FOR /L %%N IN (1,2,10) DO (
+	set /a M = %%N + 3
+	ECHO Training %%N  >> AllAproxResults.txt
+	python pacman.py -p ApproximateQAgent -a extractor=SimpleExtractor,epsilon=0.5,alpha=0.2 -x %%N -n !M! -l mediumClassic |findstr "Win Rate" >> AllAproxResults.txt
+)
+
+ECHO epsilon=0.8,alpha=0.5 >> AllAproxResults.txt
+FOR /L %%N IN (1,2,10) DO (
+	set /a M = %%N + 3
+	ECHO Training %%N  >> AllAproxResults.txt
+	python pacman.py -p ApproximateQAgent -a extractor=SimpleExtractor,epsilon=0.5,alpha=0.5 -x %%N -n !M! -l mediumClassic |findstr "Win Rate" >> AllAproxResults.txt
 )
 
 ECHO epsilon=0.8,alpha=0.8 >> AllAproxResults.txt
 FOR /L %%N IN (1,2,10) DO (
 	set /a M = %%N + 3
 	ECHO Training %%N  >> AllAproxResults.txt
-	python pacman.py -p ApproximateQAgent -a extractor=SimpleExtractor,epsilon=0.8,alpha=0.8 -x %%N -n !M! -l mediumGrid |findstr "Win Rate" >> AllAproxResults.txt
+	python pacman.py -p ApproximateQAgent -a extractor=SimpleExtractor,epsilon=0.5,alpha=0.8 -x %%N -n !M! -l mediumClassic |findstr "Win Rate" >> AllAproxResults.txt
 )
--- מטלה 3/reinforcement/runner.bat
+++ מטלה 3/reinforcement/runner.bat
@@ -1,11 +1,18 @@
 @ECHO OFF
 setlocal EnableDelayedExpansion
-
+::========================================= epsilon 0.2
 ECHO epsilon=0.2,alpha=0.2 >> AllResults.txt
 FOR /L %%N IN (250,250,2000) DO (
 	set /a M = %%N + 3
 	ECHO Training %%N  >> AllResults.txt
 	python pacman.py -p PacmanQAgent -x %%N -n !M! -l smallGrid -a epsilon=0.2,alpha=0.2 |findstr "Win Rate" >> AllResults.txt
+)
+
+ECHO epsilon=0.2,alpha=0.5 >> AllResults.txt
+FOR /L %%N IN (250,250,2000) DO (
+	set /a M = %%N + 3
+	ECHO Training %%N  >> AllResults.txt
+	python pacman.py -p PacmanQAgent -x %%N -n !M! -l smallGrid -a epsilon=0.2,alpha=0.5 |findstr "Win Rate" >> AllResults.txt
 )
 
 ECHO epsilon=0.2,alpha=0.8 >> AllResults.txt
@@ -14,8 +21,29 @@ FOR /L %%N IN (250,250,2000) DO (
 	ECHO Training %%N  >> AllResults.txt
 	python pacman.py -p PacmanQAgent -x %%N -n !M! -l smallGrid -a epsilon=0.2,alpha=0.8 |findstr "Win Rate" >> AllResults.txt
 )
+::========================================= epsilon 0.5
+ECHO epsilon=0.5,alpha=0.2 >> AllResults.txt
+FOR /L %%N IN (250,250,2000) DO (
+	set /a M = %%N + 3
+	ECHO Training %%N  >> AllResults.txt
+	python pacman.py -p PacmanQAgent -x %%N -n !M! -l smallGrid -a epsilon=0.5,alpha=0.2 |findstr "Win Rate" >> AllResults.txt
+)
 
-ECHO epsilon=0.8,alpha=0.2 >> AllResults.txt
+ECHO epsilon=0.5,alpha=0.5 >> AllResults.txt
+FOR /L %%N IN (250,250,2000) DO (
+	set /a M = %%N + 3
+	ECHO Training %%N  >> AllResults.txt
+	python pacman.py -p PacmanQAgent -x %%N -n !M! -l smallGrid -a epsilon=0.5,alpha=0.5 |findstr "Win Rate" >> AllResults.txt
+)
+
+ECHO epsilon=0.5,alpha=0.8 >> AllResults.txt
+FOR /L %%N IN (250,250,2000) DO (
+	set /a M = %%N + 3
+	ECHO Training %%N  >> AllResults.txt
+	python pacman.py -p PacmanQAgent -x %%N -n !M! -l smallGrid -a epsilon=0.5,alpha=0.8 |findstr "Win Rate" >> AllResults.txt
+)
+::========================================= epsilon 0.8
+ECHO epsilon=0.5,alpha=0.2 >> AllResults.txt
 FOR /L %%N IN (250,250,2000) DO (
 	set /a M = %%N + 3
 	ECHO Training %%N  >> AllResults.txt
@@ -26,10 +54,10 @@ ECHO epsilon=0.5,alpha=0.5 >> AllResults.txt
 FOR /L %%N IN (250,250,2000) DO (
 	set /a M = %%N + 3
 	ECHO Training %%N  >> AllResults.txt
-	python pacman.py -p PacmanQAgent -x %%N -n !M! -l smallGrid -a epsilon=0.5,alpha=0.5 |findstr "Win Rate" >> AllResults.txt
+	python pacman.py -p PacmanQAgent -x %%N -n !M! -l smallGrid -a epsilon=0.8,alpha=0.5 |findstr "Win Rate" >> AllResults.txt
 )
 
-ECHO epsilon=0.8,alpha=0.8 >> AllResults.txt
+ECHO epsilon=0.5,alpha=0.8 >> AllResults.txt
 FOR /L %%N IN (250,250,2000) DO (
 	set /a M = %%N + 3
 	ECHO Training %%N  >> AllResults.txt
